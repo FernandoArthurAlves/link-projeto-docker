2a33c2b5a06e0b128061b5c50b8f8d4efb5f5d1b
FernandoArthurAlves <rbdart@gmail.com>
2023-02-17 05:05:19 +0000

    Commit dos arquivos para o projeto 3 de linux

---
+++ projeto.linux.docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Baixando arquivos..."
+
+cd /projeto3
+wget https://github.com/denilsonbonatti/toshiro-shibakita/archive/refs/heads/main.zip
+unzip main.zip
+
+echo "Finalizado!"
